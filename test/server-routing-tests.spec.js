f792ea8f68f7290dee53402a1abda8522a5d07b3
Joe Morris <morris.josephd@gmail.com>
2016-01-15 19:55:06 -0500

    update mocha tests to test server

--- test/server-routing-tests.spec.js
+++ test/server-routing-tests.spec.js
@@ -1,38 +1,42 @@
-require('./helpers/chai');
+/* global define, it, describe, beforeEach, afterEach, expect, assert */
+require('./helpers/chai')
 
-var app = require('../app');
-var request = require('request');
+var app = require('../app')
+var request = require('request')
 
-var testPort = 8000;
-var testServer;
+var testPort = 8000
+var testServer
 
-describe("App server routing", function() {
+describe('App server routing', function () {
 
-  beforeEach(function(done) {
-    testServer = app.listen(testPort, function() {
-      done();
+  beforeEach(function (done) {
+    testServer = app.listen(testPort, function () {
+      done()
     })
-        .on('error', function(err) {
-          console.log(err);
-        });
-  });
+        .on('error', function (err) {
+          console.log(err)
+        })
+  })
 
-  afterEach(function() {
-    testServer.close();
-  });
+  afterEach(function () {
+    testServer.close()
+  })
 
-  it('should return a response code of "404" when a url does not exits', function(done) {
-    request.get('http://localhost:8000/badUrl', function(err, res, body) {
-      expect(res.statusCode).to.equal(404);
-      done();
-    });
-  });
+  it('should return a response code of "404" when a url does not exits', function (done) {
+    request.get('http://localhost:8000/badUrl', function (err, res, body) {
+      if (err) {}
+      expect(res.statusCode).to.equal(404)
+      done()
+    })
+  })
 
-  it('should return a response code of "200" for the home page', function(done) {
-    request.get('http://localhost:8000/', function(err, res, body) {
-      expect(res.statusCode).to.equal(200);
-      done();
-    });
-  });
+  it('should return a response code of "200" for the home page', function (done) {
+    request.get('http://localhost:8000/', function (err, res, body) {
+      //  expect(res.statusCode).(404)
+      if (err) {}
+      expect(res.statusCode).to.equal(200)
+      done()
+    })
+  })
 
-});
+})
