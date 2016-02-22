7c9002dd268b644d5fdc05d9211baa7ea47081b2
Joe Morris <morris.josephd@gmail.com>
2016-02-21 21:38:40 -0500

    spuash this - trying dependency injetion on user controller

--- app_api/controllers/user.js
+++ app_api/controllers/user.js
@@ -1,23 +1,23 @@
-var mongoose = require('mongoose');
-var User = mongoose.model('User');
+//var mongoose = require('mongoose');
+//var User = mongoose.model('User');
 var rs = require('../services/response-service');
 
-exports.createUser = function(req, res) {
-
-  User.findOne({username: req.body.username}, function(err, user) {
-    if (!user) {
-      User.create(newUser(req), function(err, newUser) {
-        if (err) {
-          rs.sendJsonResponse(res, 400, err);
-        }
-        rs.sendJsonResponse(res, 201, newUser);
-      });
-    } else {
-      var message = {errorMessage: 'Username already exists'};
-      rs.sendJsonResponse(res, 409, message);
-    }
-  });
-
+exports.createUser = function(User) {
+  return function(req, res) {
+    User.findOne({username: req.body.username}, function(err, user) {
+      if (!user) {
+        User.create(newUser(req), function(err, newUser) {
+          if (err) {
+            rs.sendJsonResponse(res, 400, err);
+          }
+          rs.sendJsonResponse(res, 201, newUser);
+        });
+      } else {
+        var message = {errorMessage: 'Username already exists'};
+        rs.sendJsonResponse(res, 409, message);
+      }
+    });
+  }
 };
 
 function newUser(req) {
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
-var user = require('./../controllers/user');
+var user = require('./../controllers/user')(User);
 var home = require('./../controllers/home');
 
 router.get('/', home.getHome);
